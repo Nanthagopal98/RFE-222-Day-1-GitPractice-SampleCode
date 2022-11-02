51a854ec1329a08bd693a0291e510cd489eef722
unknown <s.nanthagopal1998@gmail.com>
2022-11-02 19:23:11 +0530

    [Nantha] - Calculator with Add Number Done

--- SampleCode/Program.cs
+++ SampleCode/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SampleCode;
+
 Console.WriteLine("Hello, World!");
+Calculator calculator = new Calculator();
+Console.WriteLine("Enter First Number");
+calculator.a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+calculator.b = Convert.ToInt32(Console.ReadLine());
+calculator.AddTwoNumber();
